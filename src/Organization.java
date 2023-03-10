58702fc7b9dc2a5d34262888f61eba28eb28527f
Ahamed Careem <68745704+amcareem@users.noreply.github.com>
2023-03-10 21:50:00 +0530

    Add files via upload

---
+++ src/Organization.java
@@ -0,0 +1,11 @@
+// Written by Ahamed Careem (Github profile: amcareem)
+public class Organization {
+    private String name;
+    String category;
+
+    public Organization(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+}
+
---
+++ src/Supplier.java
@@ -0,0 +1,26 @@
+// Written by Ahamed Careem (Github profile: amcareem)
+public class Supplier{
+    private String name;
+    public String category;
+    private float price;
+
+    public void setPrice(float price) {
+
+        this.price = price;
+    }
+
+    public Supplier(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public Supplier() {
+        // default constructor
+    }
+    public void show(){
+        System.out.println(name +" place bid for Rs:"+price+"/=");
+    }
+    public void showLowest(){
+        System.out.println("Tender in "+category+"category goes to "+ name +" with Rs:"+price+"/=.");
+    }
+}
